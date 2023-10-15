7120ecac25c835b45e4f4c275483a0414608c6db
David Robbins <robbins83@gmail.com>
2023-10-15 17:19:17 -0400

    assign an issue to a user

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -8,13 +8,13 @@ export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  // const session = await getServerSession(authOptions);
-  // if (!session) return NextResponse.json({}, { status: 401 });
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
 
   const body = await request.json();
   const validation = patchIssueSchema.safeParse(body);
   if (!validation.success)
-    return NextResponse.json(validation.error.errors, { status: 400 });
+    return NextResponse.json(validation.error.format(), { status: 400 });
 
   const { assignedToUserId, title, description } = body;
 
