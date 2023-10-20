710586dfbf032a28e75cfd46b2547e9cee85d96e
David Robbins <robbins83@gmail.com>
2023-10-20 08:52:59 -0400

    build issue chart component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/prisma/client";
 import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
+import IssueChart from "./IssueChart";
 
 export default async function Home() {
   const open = await prisma.issue.count({ where: { status: "OPEN" } });
@@ -10,6 +11,7 @@ export default async function Home() {
   const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
 
   return (
-  <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+  // <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+  <IssueChart open={open} inProgress={inProgress} closed={closed} />
   );
 }
